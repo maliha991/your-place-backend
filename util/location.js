213a1bf42b6fa28195ea190e2f651cd8af52f6f3
maliha991 <63651648+maliha991@users.noreply.github.com>
2021-06-05 20:01:37 +0600

    Update location.js

--- util/location.js
+++ util/location.js
@@ -5,35 +5,35 @@ const HttpError = require("../models/http-error");
 const API_KEY = "AIzaSyBqvYG1FVrSC4Kyqb_3a4pp_mjgAWYIoI0";
 
 async function getCoordsForAddress(address) {
-	// return {
-	// 	lat: 40.7484474,
-	// 	lng: -73.9871516,
-	// };
+	return {
+		lat: 40.7484474,
+		lng: -73.9871516,
+	};
 
-	try {
-		const response = await axios.get(
-			`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
-				address
-			)}&key=${API_KEY}`
-		);
+// 	try {
+// 		const response = await axios.get(
+// 			`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+// 				address
+// 			)}&key=${API_KEY}`
+// 		);
 
-		const data = response.data;
+// 		const data = response.data;
 
-		if (!data || data.status === "ZERO_RESULTS") {
-			throw new HttpError(
-				"Could not find location for the specified address.",
-				422
-			);
-		}
+// 		if (!data || data.status === "ZERO_RESULTS") {
+// 			throw new HttpError(
+// 				"Could not find location for the specified address.",
+// 				422
+// 			);
+// 		}
 
-		console.log(data);
+// 		console.log(data);
 
-		const coordinates = data.results[0].geometry.location;
+// 		const coordinates = data.results[0].geometry.location;
 
-		return coordinates;
-	} catch (error) {
-		console.log(error);
-	}
+// 		return coordinates;
+// 	} catch (error) {
+// 		console.log(error);
+// 	}
 }
 
 module.exports = getCoordsForAddress;
